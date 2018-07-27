64422f6a940895edbe8f52c4db1b185154bb4457
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2018-07-27 10:54:17 -0700

    add integration with cloud firestore for storing shelters

--- backend.py
+++ backend.py
@@ -1,16 +1,34 @@
 from flask import Flask, render_template
 import firebase_admin
+import os
 from firebase_admin import credentials
+from google.cloud import firestore
 
-cred = credentials.Certificate("config/firebasesecrets.json")
-firebase_admin.initialize_app(cred)
+#cred = credentials.Certificate("config/firebasesecrets.json")
+#firebase_admin.initialize_app(cred)
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'config/firebasesecrets.json'
 
 app = Flask(__name__)
+db = firestore.Client()
 
 @app.route('/')
 def home():
-    return render_template('homepage.html', test="Hello")
+    return render_template('homepage.html', shelters=getAllShelters())
 
 @app.route('/signup')
 def signup():
     return render_template('signuppage.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+def getAllShelters():
+    shelters = []
+    # Then query for documents
+    shelters_ref = db.collection(u'shelters')
+    shelter_docs = shelters_ref.get()
+    for shelter in shelter_docs:
+        shelters.append(shelter.to_dict())
+        
+    return shelters
