681a52fd071905c71a80cbbe2c01222529d25159
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2018-08-09 14:58:13 -0700

    add other method parameter

--- backend.py
+++ backend.py
@@ -131,7 +131,7 @@ def account():
             flash('Error: The number of beds available cannot be higher than the shelter capacity.', 'alert-danger')
             return redirect('/account')
 
-        if getNearestPlaceWithName(constructAddress(formData)) == None:
+        if getNearestPlaceWithName(constructAddress(formData), formData['name']) == None:
             flash('Error: The address you provided could not be found in the Google Maps database.', 'alert-danger')
             return redirect('/account')
 
